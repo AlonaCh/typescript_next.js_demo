a9c5a5fb319eab043a30c457548f767e4c0a99ad
Alona Chubenko <aliona.chubenko@gmail.com>
2024-03-08 11:00:27 +0200

    First init

---
+++ src/app/countries/CountriesDetail.tsx
@@ -0,0 +1,18 @@
+const CountriesDetails = () =>{
+
+    interface Country {
+        name: string;
+        capital: string;
+        population: number;
+        area: number;
+        flag: string;
+
+    }
+
+    return (
+        <div>
+            <h1>Country Details</h1>
+        </div>
+    )
+}
+export default CountriesDetails;
